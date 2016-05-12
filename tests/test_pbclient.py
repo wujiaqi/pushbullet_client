82424211f381b80d178a09395af5ea56cd803ac7
Jiaqi Wu <wooqiaoqi@gmail.com>
2016-05-11 23:30:48 -0700

    add test message for environment variable

--- tests/test_pbclient.py
+++ tests/test_pbclient.py
@@ -7,6 +7,9 @@ BR3_TEST_CHANNEL="br3foodtrucks_dev"
 
 def test_push_integration():
     token = os.environ.get('PUSHBULLET_TOKEN')
+    if token is None:
+    	print "must set PUSHBULLET_TOKEN env variable before running this test"
+    	assert 0
     test_id = str(uuid.uuid4())
     pbclient.push_note_to_channel(BR3_TEST_CHANNEL, "integration test", test_id, token)
     channel_info = pbclient.get_channel_info(BR3_TEST_CHANNEL, token)
